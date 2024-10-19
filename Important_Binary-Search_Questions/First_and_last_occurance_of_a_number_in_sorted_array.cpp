5d1b16580420ba75cab2dbf34685ea1f82cd0b35
Anupriya Chandrasekhar <106677023+anupriyaC2957@users.noreply.github.com>
2024-10-19 15:10:22 +0530

    Add files via upload

---
+++ Important_Binary-Search_Questions/First_and_last_occurance_of_a_number_in_sorted_array.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+}
+
+int firstOcurrance(int arr[],int n,int key)
+{
+    int start=0;
+    int end=n-1;
+    int ans =-1;
+    while(start<=end)
+    {
+       int mid=start+(end-start)/2;
+       if(arr[mid]==key)
+       {
+            ans=mid; 
+            end = mid-1;
+       }
+       else if(arr[mid]<key)
+       {
+            start=mid+1;
+       }
+       else //arr[mid]>key
+       {
+            end=mid-1;
+       }
+       
+    }
+    return ans;
+
+}
+int LastOcurrance(int arr[],int n,int key)
+{
+    int start=0;
+    int end=n-1;
+    int ans =-1;
+    while(start<=end)
+    {
+       int mid=(end+start)/2;
+       if(arr[mid]==key)
+       {
+            ans=mid; 
+            start = mid+1;
+       }
+       else if(arr[mid]<key)
+       {
+            start=mid+1;
+       }
+       else //arr[mid]>key
+       {
+            end=mid-1;
+       }
+       
+    }
+    return ans;
+
+}
+
+int main()
+{
+    int arr[10]={1,2,2,3,3,3,3,4,4,5};
+    cout<<"First Occurance of 3 is at Index: "<< firstOcurrance(arr,10,3)<<endl;
+    cout<<"Last Occurance of 3 is at Index: "<< LastOcurrance(arr,10,3)<<endl;
+  
+   
+   return 0;
+
+}
