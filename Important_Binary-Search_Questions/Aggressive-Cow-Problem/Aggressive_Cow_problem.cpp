914148bc20ebd88a5b5534c38d733b4cb9eccafc
Anupriya Chandrasekhar <106677023+anupriyaC2957@users.noreply.github.com>
2024-11-02 09:58:38 +0530

    Update Aggressive_Cow_problem.cpp

--- Important_Binary-Search_Questions/Aggressive-Cow-Problem/Aggressive_Cow_problem.cpp
+++ Important_Binary-Search_Questions/Aggressive-Cow-Problem/Aggressive_Cow_problem.cpp
@@ -89,6 +89,7 @@ int main()
         cin>>stalls[i];
     cout<<endl;
     vector<pair<int,int>>final_position;
+    // basically store the position and the distance in the final_position--> This is additional part !
     
     int result = aggressiveCows(stalls,k,final_position);
     cout<<"Minimum Distance Assigned Between The Stalls With Largest Distance as: "<<result<<endl;
@@ -101,48 +102,5 @@ int main()
     return 0;
 }
 
-
-
-
-/*Problem statement
-You are given an array 'arr' consisting of 'n' integers which denote the position of a stall.
-You are also given an integer 'k' which denotes the number of aggressive cows.
-
-You are given the task of assigning stalls to 'k' cows 
-such that the minimum distance between any two of them is the maximum possible.
-
-Print the maximum possible minimum distance.
-
-Example:
-Input: 'n' = 3, 'k' = 2 and 'arr' = {1, 2, 3}
-
-Output: 2
-
-Explanation: The maximum possible minimum distance will be 2 when 2 cows are placed at positions {1, 3}. 
-Here distance between cows is 2.
-Detailed explanation ( Input/output format, Notes, Images )
-Sample Input 1 :
-6 4
-0 3 4 7 10 9
-Sample Output 1 :
-3
-
-Explanation to Sample Input 1 :
-The maximum possible minimum distance between any two cows will be 3 when 4 cows are placed at positions {0, 3, 7, 10}. 
-Here distance between cows are 3, 4 and 3 respectively.
-Sample Input 2 :
-5 2
-4 2 1 3 6
-Sample Output 2 :
-5
-
-Expected time complexity:
-Can you solve this in O(n * log(n)) time complexity?
-
-Constraints :
-2 <= 'n' <= 10 ^ 5
-2 <= 'k' <= n
-0 <= 'arr[i]' <= 10 ^ 9
-Time Limit: 1 sec.*/
 
 
