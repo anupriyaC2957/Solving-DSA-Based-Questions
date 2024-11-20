88433e748e2b1de87499396b86d606aa1f25a68a
Anupriya Chandrasekhar <106677023+anupriyaC2957@users.noreply.github.com>
2024-11-20 19:12:43 +0530

    Update Finding_The_First_Maximum_Occuring_Character_CodeNinja.cpp

--- Array_Questions/Maximum_Occuring_Character_In_A_String/Finding_The_First_Maximum_Occuring_Character_CodeNinja.cpp
+++ Array_Questions/Maximum_Occuring_Character_In_A_String/Finding_The_First_Maximum_Occuring_Character_CodeNinja.cpp
@@ -1,5 +1,6 @@
 /*sentence: test --> output: t 
  sentence: sampletest --> output: s
+ sentence: SampLETest --> output: S
  sentence: aabbb --> output: b
  sentence: yyxxxy --> output: y
  sentence: xxyyyx --> output: x
@@ -10,21 +11,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+char ToLower(char ch)  // non-Case-Sensitive
+{
+    if(ch>='a' && ch<= 'z')
+    {
+        return ch;
+    }
+    else
+    {
+        char temp;
+        temp = ch -'A'+'a';
+        return temp;
+    }
+}
+
 char highestOccurringChar(char input[]) {
    
-    unordered_map <char,int> ans;
+    unordered_map <char,int> ans; // unordered map --> iteration order is not fixed --> change can occur--> do not depend on the iteration order
     char finals;
+    char ch;
     int max= -1;
     int n = strlen(input);
 
     for(int i=0; i<n; i++)
     {
-        ans[input[i]]++;
+        ch = ToLower(input[i]);
+        ans[ch]++;
     }
 
     for(int i=0;i<n; i++)
     {
-        char current = input[i];
+        char current = ToLower(input[i]);
         if(ans[current]> max)
         {
             max= ans[current];
@@ -46,8 +63,8 @@ int main()
     getline(cin, sentence);
 
     int n = sentence.length();
-    //cout<<"value of n is: "<<n<<endl;
-    //cout<<"Value of n+1 is:"<<n+1<<endl;
+    cout<<" Value of n is: "<<n<<endl;
+    cout<<" Value of n+1 is:"<<n+1<<endl;
 
     char s[n+1];
 
